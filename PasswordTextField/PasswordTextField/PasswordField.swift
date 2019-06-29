18ca29c5ed3a75d8edfb4155c38eac8a6e4eaf73
Dongwoo Pae <dongwoopae@gmail.com>
2019-06-29 12:36:31 -0700

    finished setup() using CGRect. will implement NSLayoutConstraint or NSLayoutAnchor

--- PasswordTextField/PasswordTextField/PasswordField.swift
+++ PasswordTextField/PasswordTextField/PasswordField.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+enum StrrengthLevel : String {
+    case weak = "Too weak"
+    case medium = "Could be stronger"
+    case strong = "Strong password"
+}
+
+
 class PasswordField: UIControl {
     
     // Public API - these properties are used to fetch the final password and strength values
@@ -39,10 +46,58 @@ class PasswordField: UIControl {
     private var strengthDescriptionLabel: UILabel = UILabel()
     
     func setup() {
+        
         // Lay out your subviews here
         
+        // 1. titleLabel
+        titleLabel.frame = CGRect(x: 0, y: 0, width: 80, height: 40)
+        titleLabel.text = "ENTER PASSWORD"
+        titleLabel.textColor = labelTextColor
+        titleLabel.font = labelFont
         addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 2. textField
+        textField.frame = CGRect(x: 0, y: 25.0, width: 330, height: textFieldContainerHeight)
+        textField.textColor = labelTextColor
+        textField.layer.borderColor = textFieldBorderColor.cgColor
+        textField.layer.cornerRadius = 8
+        textField.layer.borderWidth = 2.0
+        
+        
+        addSubview(textField)
+        // 2-1 button
+        showHideButton.frame = CGRect(x: 290.0, y: 10.0, width: 30.0, height: 30.0)
+        showHideButton.setImage(UIImage(named: "eyes-closed"), for: .normal)
+        showHideButton.layer.borderColor = UIColor.red.cgColor
+        showHideButton.addTarget(self, action: #selector(imageTapped), for: .touchUpInside)
+        textField.addSubview(showHideButton)
+        
+        // 3. indicator bars
+        weakView.frame = CGRect(origin: CGPoint(x: 0, y: 90.0), size: colorViewSize)
+        weakView.backgroundColor = weakColor
+        addSubview(weakView)
+        
+        mediumView.frame = CGRect(origin: CGPoint(x: colorViewSize.width + 5, y: 90.0), size: colorViewSize)
+        mediumView.backgroundColor = .lightGray
+        addSubview(mediumView)
+        
+        strongView.frame = CGRect(origin: CGPoint(x: colorViewSize.width * 2 + 5 + 5, y: 90.0), size: colorViewSize)
+        strongView.backgroundColor = .lightGray
+        addSubview(strongView)
+        
+        // 4. indicator label
+        strengthDescriptionLabel.frame = CGRect(origin: CGPoint(x: colorViewSize.width * 3 + 5 + 5 + 7, y: 90.0), size: CGSize(width: colorViewSize.width * 2, height: 10.00))
+        strengthDescriptionLabel.textColor = labelTextColor
+        strengthDescriptionLabel.text = StrrengthLevel.weak.rawValue
+        strengthDescriptionLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        addSubview(strengthDescriptionLabel)
+        
+    }
+    
+    @objc func imageTapped() {
+        //change image and show password
+        showHideButton.setImage(UIImage(named: "eyes-open"), for: .normal)
     }
     
     required init?(coder aDecoder: NSCoder) {
--- PasswordTextField/PasswordTextField/ViewController.swift
+++ PasswordTextField/PasswordTextField/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBAction func passwordViews(_ sender: PasswordField) {
+        
+    }
     // For use in the stretch goal
     //
     // Uncomment this entire method, then run the app.
@@ -19,12 +22,12 @@ class ViewController: UIViewController {
     // the little cloud download button to install it. Then just stop the app
     // and comment this method out again. This step only needs to run once.
     
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
 //        // Uncomment this portion to set up the dictionary
 //        let str = "lambda"
 //        let referenceVC = UIReferenceLibraryViewController(term: str)
 //        present(referenceVC, animated: true, completion: nil)
-//    }
+    }
 }
